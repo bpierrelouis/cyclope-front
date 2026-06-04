a7c9365ed80cfcd698a08fc6d6b55abd42393fa0
Tony <constanttony84@gmail.com>
2026-06-04 15:14:31 +0200

    feat: Ajout du tri des missions par zone de recherche

--- src/views/missionList/MissionListScreen.jsx
+++ src/views/missionList/MissionListScreen.jsx
@@ -5,12 +5,21 @@ import { StatusTabs } from './StatusTabs';
 
 export function MissionListScreen() {
     const [status, setStatus] = useState(STATUS[0]);
+    const [search, setSearch] = useState('');
 
     return (
         <main className='flex flex-col gap-6 m-10'>
-            <h2 className='font-medium text-xl'>Missions</h2>
-            <StatusTabs statusState={[status, setStatus]} />
-            <MissionList status={status} />
+            <div className='flex items-center justify-between'>
+                <StatusTabs statusState={[status, setStatus]} />
+                <input
+                    type='text'
+                    placeholder='Rechercher...'
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    className='input input-sm input-bordered w-48'
+                />
+            </div>
+            <MissionList status={status} search={search} />
         </main>
     );
 }
