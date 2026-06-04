f0a47242b4ca666beeb46d5258b7f2e20ed1f619
Tony <constanttony84@gmail.com>
2026-06-04 15:25:55 +0200

    feat: Ajout du store zustand pour les param-tres globaux

--- src/views/new/waitingZone/config/FileConfig.jsx
+++ src/views/new/waitingZone/config/FileConfig.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo } from 'react';
-import { useMissionCreationStore } from '../../../../stores';
+import { useDefaultConfigStore, useMissionCreationStore } from '../../../../stores';
 import { ConfidenceThresholdChange } from './ConfidenceThresholdChange';
 import { ObjectDetectionField } from './ObjectDetectionField';
 import { ProcessingIntervalField } from './ProcessingIntervalField';
@@ -9,16 +9,17 @@ export function FileConfig(props) {
     const { file } = props;
 
     const { configs, updateConfig } = useMissionCreationStore();
+    const defaultConfig = useDefaultConfigStore();
 
     const config = useMemo(() => configs[file.id], [configs, file.id]);
 
     useEffect(() => {
         if (config) return;
         updateConfig(file.id, {
-            processingInterval: 3,
-            objectDetectionEnabled: false,
-            confidenceThreshold: 50,
-            processingLevel: 'medium',
+            processingInterval: Math.min(defaultConfig.processingInterval, file.duration ?? Infinity),
+            objectDetectionEnabled: defaultConfig.objectDetectionEnabled,
+            confidenceThreshold: defaultConfig.confidenceThreshold,
+            processingLevel: defaultConfig.processingLevel,
         });
     }, [config, file.id, updateConfig]);
 
