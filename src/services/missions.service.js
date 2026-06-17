fb309b729db6c78aa57e0be62c66afcf6bb8bcb6
Pierre-Louis BAELDE <pl.baelde@gmail.com>
2026-06-17 18:07:09 +0200

    fix: suppression du endpoint particulier pour la création d'une mission

--- src/services/missions.service.js
+++ src/services/missions.service.js
@@ -1,17 +1,8 @@
 import { Mission } from '../models';
 import { createCrudService } from './crud.factory';
-import { httpRequest } from './httpClient';
 
 export const missionsResourceName = 'missions';
 
 const service = createCrudService(missionsResourceName, Mission.mapper);
 
-const create = (payload) => httpRequest('new', {
-    method: 'POST',
-    body: payload,
-}).then(Mission.mapper);
-
-export const missionsService = {
-    ...service,
-    create,
-};
+export const missionsService = service;
